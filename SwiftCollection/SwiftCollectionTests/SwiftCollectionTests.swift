7fde804766d4ed42085dbda39897123dca17aecf
Warner Zee <wyzzarz@gmail.com>
2017-02-28 21:57:43 -0800

    Add project.

---
+++ SwiftCollection/SwiftCollectionTests/SwiftCollectionTests.swift
@@ -0,0 +1,37 @@
+//
+//  SwiftCollectionTests.swift
+//
+//  Copyright 2017 Warner Zee
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import XCTest
+@testable import SwiftCollection
+
+class SwiftCollectionTests: XCTestCase {
+  
+  override func setUp() {
+    super.setUp()
+  }
+  
+  override func tearDown() {
+    super.tearDown()
+  }
+  
+  func testBundle() {
+    XCTAssertTrue(SwiftCollection.bundleId.lengthOfBytes(using: .utf8) > 0)
+    XCTAssertNotNil(SwiftCollection.bundle)
+  }
+  
+}
